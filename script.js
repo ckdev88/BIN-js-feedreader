5ba6100a3b9302b3bc365ae7100b1d116d2b9acc
ckdev88 <ckdev88@outlook.com>
2023-09-30 17:38:49 -0300

    apply stricter load filtering based on recent years

--- script.js
+++ script.js
@@ -19,52 +19,83 @@ const feedlist = [
 	'https://andrewkelley.me/rss.xml'
 ];
 
-const loopfeeds = () => {
-	const sanitizedDateTime = (datetime) => {
-		datetime = datetime.replace('+0000', '');
-		datetime = datetime.replace('00:00:00', '');
-		datetime = datetime.replace('TZ', '');
-		datetime = datetime.replace('GMT', '');
-		return datetime;
-	}
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const dateFilters = ['+0000', '00:00:00', 'TZ', 'GMT', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', ','];
 
-	const articleLink = (title, link, date) => {
-		return `<a href="${link}" target="_blank">${title} <span class="pubdate">${date}</span></a>`;
+function cleanUglyDate(datetime) {
+	dateFilters.forEach(replacethem);
+	function replacethem(item) {
+		datetime = datetime.replace(item, '');
 	}
+	return datetime;
+}
+
+const loopfeeds = () => {
+	const articleLink = (title, link, date) => {
+		date = cleanUglyDate(date);
+		return `<a href="${link}" target="_blank">${title} <span class="pubdate">${date} - ${currentYear}</span></a>`;
+	}
+	let tmpurl;
 	for (feed of feedlist) {
+		const feedUri = feed;
 		fetch(feed)
 			.then(response => response.text())
 			.then(string => new window.DOMParser().parseFromString(string, 'text/xml'))
 			.then(data => {
 				let loopfeedsHtml = ``;
-				let feedTitle = data.querySelector('title').innerHTML.replace('<![CDATA[', '').replace(']]>', '');
+				const feedTitle = data.querySelector('title').innerHTML.replace('<![CDATA[', '').replace(']]>', '');
 				loopfeedsHtml += `<h2>${feedTitle}</h2>`;
 				let items = data.querySelectorAll('item');
-
 				if (items.length > 0) {
+					tmpurl = feed;
+					const firstItemDate = items[0].querySelector('pubDate').innerHTML;
 					count = 0;
-					items.forEach(item => {
-						if (count < 5) {
-							count += 1;
-							let pubDate = sanitizedDateTime(item.querySelector('pubDate').innerHTML);
-							let title = item.querySelector('title').innerHTML.replace('<![CDATA[', '').replace(']]>', '');
-							let link = item.querySelector('link').innerHTML;
-							loopfeedsHtml += articleLink(title, link, pubDate);
-						}
-					});
+					if (
+						firstItemDate.indexOf(currentYear, firstItemDate) >= 0
+						||
+						firstItemDate.indexOf(currentYear - 1, firstItemDate) >= 0
+					) {
+						items.forEach(item => {
+							if (count < 5) {
+								let itemDate = item.querySelector('pubDate').innerHTML;
+								if (
+									itemDate.indexOf(currentYear, itemDate) > -1
+									|| itemDate.indexOf((currentYear - 1), itemDate) > -1
+								) {
+									count += 1;
+									let pubDate = cleanUglyDate(item.querySelector('pubDate').innerHTML);
+									let title = item.querySelector('title').innerHTML.replace('<![CDATA[', '').replace(']]>', '');
+									let link = item.querySelector('link').innerHTML;
+									loopfeedsHtml += articleLink(title, link, pubDate);
+								}
+							}
+						});
+					}
+					else loopfeedsHtml += `${feedTitle} is all old, time to clean up? Source: ${feedUri}`;
 				}
 				items = data.querySelectorAll('entry');
 				if (items.length > 0) {
+					const firstItemDate = items[0].querySelector('updated').innerHTML;
 					count = 0;
-					items.forEach(item => {
-						if (count < 5) {
-							count += 1;
-							let pubDate = sanitizedDateTime(item.querySelector('updated').innerHTML);
-							let title = item.querySelector('title').innerHTML.replace('<![CDATA[', '').replace(']]>', '');
-							let link = item.querySelector('link').innerHTML;
-							loopfeedsHtml += articleLink(title, link, pubDate);
-						}
-					});
+
+					if (firstItemDate.indexOf(currentYear, firstItemDate) >= 0) {
+						items.forEach(item => {
+							if (count < 5) {
+								if (
+									itemDate.indexOf(currentYear, itemDate) > -1
+									|| itemDate.indexOf((currentYear - 1), itemDate) > -1
+								) {
+									count += 1;
+									let pubDate = cleanUglyDate(item.querySelector('updated').innerHTML);
+									let title = item.querySelector('title').innerHTML.replace('<![CDATA[', '').replace(']]>', '');
+									let link = item.querySelector('link').innerHTML;
+									loopfeedsHtml += articleLink(title, link, pubDate);
+								}
+							}
+						});
+					}
+					if (count === 0) loopfeedsHtml += `${feedTitle} is all old, time to clean up? Source: ${feedUri}`;
 				}
 				document.getElementById('rss').innerHTML += loopfeedsHtml;
 			})
